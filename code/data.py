37acc356d7e2b7787512e5bb0136d60964c3681a
lyan68 <liu.yan@wustl.edu>
2021-11-25 16:56:09 -0600

    Update data.py

--- code/data.py
+++ code/data.py
@@ -1,5 +1,9 @@
 from utils import generate_object
+from imaging import generate_recon
 
 ## generate object
 num_objects = 500
 generate_object(num_objects)
+
+## generate reconstructed image
+generate_recon(num_objects)
