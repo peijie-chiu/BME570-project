cd3da9b09403961dbf2c979b6fcca47f5469e78f
peijie-chiu <peijie.qiu@wustl.edu>
2021-11-29 13:36:54 -0600

    update for imaging.py

--- code/imaging.py
+++ code/imaging.py
@@ -1,10 +1,28 @@
 import numpy as np
 
 
-flip_angles = (np.pi / 8) * np.arange(0, 8)
-m = (np.arange(1, 17, 1) - 8).reshape(-1, 1)
+def generate_sys_matrix(dim=64):
+    flip_angles = ((np.pi / 8) * np.arange(0, 8)).reshape(-1, 1)
+    m = (np.arange(1, 17, 1) - 8).reshape(1, -1)
 
-km_x = m * np.cos(flip_angles)
-km_y = m * np.sin(flip_angles)
-km = np.stack([km_x, km_y])
-print(km[:, 0, 0])
+    km_x = (np.cos(flip_angles) * m).reshape(-1, 1)
+    km_y = (np.sin(flip_angles) * m).reshape(-1, 1)
+    km = np.hstack([km_x, km_y])
+
+    x = np.linspace(-1/2, 1/2, dim)
+    y = np.linspace(-1/2, 1/2, dim)
+
+    xx, yy = np.meshgrid(x, y)
+    coords = np.hstack([xx.reshape(-1, 1), yy.reshape(-1, 1)])
+
+    H = np.exp(-2j * np.pi * km.dot(coords.T))
+    
+    return H
+
+def svd(H):
+    H_adjoint_H = H.T.dot(H)
+    eigen_values, eigen_vectors = np.linalg.eig(H_adjoint_H)
+    print(eigen_values)
+
+H = generate_sys_matrix()
+svd(H)
